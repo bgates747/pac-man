4c363b97578a0df0a465c9331aa658072a83ac32
Andy McCall <mailme@andymccall.co.uk>
2024-11-26 20:02:42 +0000

    chore(): start of coding

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+![Pac-Man](https://github.com/andymccall/pac-man/blob/main/assets/header.jpg?raw=true)
+
 ## Pac-Man
 
-A port of the 1980's arcade game Pac-Mac to the Agon Light 2
-
 Pac-Man, originally called Puck Man in Japan, is a 1980 maze video game developed and released by Namco for arcades. In North America, the game was released by Midway Manufacturing as part of its licensing agreement with Namco America. The player controls Pac-Man, who must eat all the dots inside an enclosed maze while avoiding four colored ghosts. Eating large flashing dots called "Power Pellets" causes the ghosts to temporarily turn blue, allowing Pac-Man to eat them for bonus points.
+
+This project intents to hold the most perfect arcade port of the 1980's arcade game Pac-Mac to the Agon Light 2 possible, within the limitations of the platform.
+
+Controls will be either by keyboard or via the [EIGHTSBITWIDE Arcade board](https://andymccall.co.uk/electronics/retrocomputing/agon/basic/2024/07/09/agon-light-2-arcade-controller-board.html).
--- src/pac-man.asm
+++ src/pac-man.asm
@@ -18,6 +18,11 @@
 .align 64
 .db "MOS",0,1
 
+    include "src/includes/system/mos_api.inc"
+    include "src/includes/screen.inc"
+    include "src/includes/cursor.inc"
+    include "src/includes/text.inc"
+
 start:
     push af
     push bc
@@ -25,10 +30,33 @@ start:
     push ix
     push iy
 
-    ld hl,program_name
-    ld bc,0
-    ld a,0
+    setScreenMode SCREENMODE_320x240x64_60HZ
+    call setScreenScaling
+
+    call cursorHide
+
+game_loop:
+
+    ; Print the high score
+    ld hl, high_score_data
+    ld bc, high_score_data_end - high_score_data
     rst.lil $18
+
+    ld a, mos_getkbmap
+	rst.lil $08
+
+    ; If the Escape key is pressed
+    ld a, (ix + $0E)    
+    bit 0, a
+    jp nz, quit
+
+    jp game_loop
+
+quit:
+    setScreenMode SCREENMODE_640x480x4_60HZ
+    call cursorFlash
+
+    printText quit_msg
 
     pop iy
     pop ix
@@ -39,5 +67,10 @@ start:
 
     ret
 
-program_name:
-    .db "Pac-Man!",13,10,0
+quit_msg:
+    .db "Thank you for playing Pac-Man!",13,10,0
+
+high_score_data:
+    .db     31, 15, 0
+    .db     "HIGH SCORE"
+high_score_data_end:
