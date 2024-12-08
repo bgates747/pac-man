0345511307838fcae8cd0c9fcc6938897fd01422
Andy McCall <mailme@andymccall.co.uk>
2024-12-08 13:39:39 +0000

    chore(): refactor, includes bgates747 converted sprites

--- src/pac-man.asm
+++ src/pac-man.asm
@@ -18,11 +18,45 @@
 .align 64
 .db "MOS",0,1
 
+; API includes
     include "src/includes/system/mos_api.inc"
-    include "src/includes/screen.inc"
-    include "src/includes/cursor.inc"
-    include "src/includes/text.inc"
-    include "src/includes/sprite.inc"
+    include "src/includes/api/vdu.inc"
+    include "src/includes/api/vdu_screen.inc"
+    include "src/includes/api/vdu_cursor.inc"
+    include "src/includes/api/vdu_buffer.inc"
+    include "src/includes/api/vdu_bitmap.inc"
+    include "src/includes/api/vdu_text.inc"
+    include "src/includes/api/sprite.inc"
+    include "src/includes/api/macro_sprite.inc"
+    include "src/includes/api/macro_text.inc"
+    include "src/includes/api/macro_bitmap.inc"
+
+; Application includes
+    include "src/includes/game/vdu_data.inc"
+    include "src/includes/game/images_sprites.inc"
+    include "src/includes/game/timer.inc"
+    include "src/includes/game/maze/maze.inc"
+
+; Character Sprites
+    include "src/includes/game/sprites/pac_man/pac_man.inc"
+    include "src/includes/game/sprites/pac_man/pac_man_life.inc"
+    include "src/includes/game/sprites/ghosts/blinky.inc"
+    include "src/includes/game/sprites/ghosts/pinky.inc"
+    include "src/includes/game/sprites/ghosts/inky.inc"
+    include "src/includes/game/sprites/ghosts/clyde.inc"
+    include "src/includes/game/sprites/ghosts/reverse.inc"
+    include "src/includes/game/sprites/fruit/fruit.inc"
+    include "src/includes/game/sprites/fruit/cherry.inc"
+    include "src/includes/game/sprites/fruit/berry.inc"
+    include "src/includes/game/sprites/fruit/orange.inc"
+    include "src/includes/game/sprites/fruit/apple.inc"
+    include "src/includes/game/sprites/fruit/melon.inc"
+    include "src/includes/game/sprites/fruit/galaxian.inc"
+    include "src/includes/game/sprites/fruit/bell.inc"
+    include "src/includes/game/sprites/fruit/key.inc"
+
+; Maze Bitmaps
+    include "src/includes/game/sprites/maze/maze_tile.inc"
 
 start:
     push af
@@ -31,13 +65,18 @@ start:
     push ix
     push iy
 
-    setScreenMode SCREENMODE_512x384x16_60HZ
-    call setScreenScaling
+    call vdu_buffer_clear_all
 
-    call cursorHide
+    ld a, VDU_SCREENMODE_512x384x64_60HZ
+    call vdu_screen_set_mode
 
-    ld hl, VDUdata                      ; address of string to use
-    ld bc, endVDUdata - VDUdata         ; length of string
+    ld a, VDU_SCALING_ON
+    call vdu_screen_set_scaling
+
+    call vdu_cursor_off
+
+    ld hl, vdu_data                      ; address of string to use
+    ld bc, vdu_data_end - vdu_data         ; length of string
     rst.lil $18                         ; Call the MOS API to send data to VDP
 
     ; Print the 1UP text
@@ -55,7 +94,19 @@ start:
     ld bc, up2_txt_end - up2_txt
     rst.lil $18
 
+
+    call draw_maze
+
+    call vdu_vblank
+
+    call vdu_refresh
+
+
+    call game_timer_tick
+
 game_loop:
+
+    call game_timer_tick
 
     ld a, mos_getkbmap
 	rst.lil $08
@@ -68,10 +119,14 @@ game_loop:
     jp game_loop
 
 quit:
-    setScreenMode SCREENMODE_640x480x4_60HZ
-    call cursorFlash
 
-    printText quit_msg
+    ld hl, VDU_SCREENMODE_640x480x4_60HZ
+    call vdu_screen_set_mode
+
+    call vdu_cursor_flash
+
+    ld hl, quit_msg
+    call vdu_text_print
 
     pop iy
     pop ix
@@ -86,142 +141,16 @@ quit_msg:
     .db "Thank you for playing Pac-Man!",13,10,0
 
 high_score_data:
-    .db     31, 15, 0
+    .db     31, 27, 3
     .db     "HIGH SCORE"
 high_score_data_end:
 
 up1_txt:
-    .db     31, 9, 0
+    .db     31, 20, 3
     .db     "1UP"
 up1_txt_end:
 
 up2_txt:
-    .db     31, 28, 0
+    .db     31, 41, 3
     .db     "2UP"
 up2_txt_end:
-
-our_sprite: EQU     0                   ; sprite ID - always start at 0 upwards
-
-VDUdata:
-    .db 23, 0, 192, 0                   ; set to non-scaled graphics
-
-    .db 23, 0, $A0                      ; clear all buffers
-    .dw -1                              ; ID, -1 for ALL as a WORD
-    .db 2                               ; clear command
-
-    .db 23, 27, 16                      ; clear all sprite and bitmap data
-
-
-    ; ------------------
-    ; create buffered bitmaps
-
-    MAKEBUFFEREDBITMAP 64002, 16, 16, "src/assets/pac-man.rgba2" 
-    MAKEBUFFEREDBITMAP 64003, 16, 16, "src/assets/blinky.rgba2" 
-    MAKEBUFFEREDBITMAP 64004, 16, 16, "src/assets/pellet.rgba2"  
-
-    ; Maze bitmaps
-    MAKEBUFFEREDBITMAP 64020, 16, 16, "src/assets/close_wall.rgba2" 
-    MAKEBUFFEREDBITMAP 64021, 16, 16, "src/assets/close_straight_corner.rgba2"
-    MAKEBUFFEREDBITMAP 64022, 16, 16, "src/assets/close_rounded_corner.rgba2"
-
-    ; ------------------
-    ; SETUP THE SPRITE
-
-    SELECT_SPRITE 0
-    CLEAR_CURRENT_SPRITE
-    ADD_SPRITE_FRAME 64002
-    ACTIVATE_SPRITES 1
-    SHOW_CURRENT_SPRITE
-
-    MOVE_SPRITE 0, 140,90
-
-    SELECT_SPRITE 1
-    CLEAR_CURRENT_SPRITE
-    ADD_SPRITE_FRAME 64003
-    ACTIVATE_SPRITES 2
-    SHOW_CURRENT_SPRITE
-
-    MOVE_SPRITE 1, 100,30
-
-    ; ------------------
-    ; just PLOT a bitmap, not sprite
-
-    .db 23, 27, $20                     ; select bitmap
-    .dw 64004                           ; id WORD
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 148, 90                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 156, 90                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 164, 90                          ; at x,y WORDs
-
-    .db 23, 27, $20                     ; select bitmap
-    .dw 64022                           ; id WORD
-    .db 25, 237                         ; plot a bitmap
-    .dw 28, 209                          ; at x,y WORDs
-
-    .db 23, 27, $20                     ; select bitmap
-    .dw 64020                           ; id WORD
-    .db 25, 237                         ; plot a bitmap
-    .dw 39, 210                          ; at x,y WORDs
-    
-    .db 25, 237                         ; plot a bitmap
-    .dw 55, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 71, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 87, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 103, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 103, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 119, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 135, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 151, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 167, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 183, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 183, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 199, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 215, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 215, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 231, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 247, 210                          ; at x,y WORDs
-
-    .db 25, 237                         ; plot a bitmap
-    .dw 263, 210                          ; at x,y WORDs
-
-    .db 23, 27, $20                     ; select bitmap
-    .dw 64022                           ; id WORD
-    .db 25, 237                         ; plot a bitmap
-    .dw 279, 209                          ; at x,y WORDs
-
-endVDUdata:
